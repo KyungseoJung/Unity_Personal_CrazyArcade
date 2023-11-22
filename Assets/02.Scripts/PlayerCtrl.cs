6f76dc0bf95a15927a2bc1526a0d149f2c2c72ba
KyungseoJung <junggs1245@gmail.com>
2023-11-22 10:47:27 +0900

    feat(PlayerCtrl.cs): Control the script so that the player slides smoothly when he encounters an obstacle #5

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -9,6 +9,7 @@ public class PlayerCtrl : MonoBehaviour // #1
     private bool dirRight = false;         // 플레이어가 바라보는 방향(오른쪽 : 1, 왼쪽 : -1)
 
     private float moveSpeed = 30f;         // 이동 속도 (50 > 20)
+    private float slideSpeed = 50f;       // #5 장애물에 닿으면 옆으로 부드럽게 지나가게 하기 위한 변수
     private float maxSpeed = 5f;
     private float h;
     private float v;
@@ -108,6 +109,28 @@ public class PlayerCtrl : MonoBehaviour // #1
         //     Flip();
         // } 
 
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+
+        if(other.gameObject.tag == "Obstacle")  // #5 장애물에 닿으면, 미끄러지듯이 지나갈 수 있도록 - 플레이어 몸을 옆으로 밀기
+        {
+            Debug.Log("//#5 장애물 부딪힘");
+
+            if(transform.position.x > other.gameObject.transform.position.x) // x 위치 값 비교해서 - 오른쪽 or 왼쪽으로 미끄러지기
+            {
+                Debug.Log("//#5 플레이어 오른쪽으로 밀기");
+
+                rBody.AddForce(Vector3.right * slideSpeed);
+            }    
+            else
+            {
+                Debug.Log("//#5 플레이어 왼쪽으로 밀기");
+
+                rBody.AddForce(Vector3.left * slideSpeed);                
+            }
+        }    
     }
 
     // void Flip() // #1 플레이어 바라보는 방향에 따라 적용  --> // #3 애니메이터로 조작해서 Flip 기능 필요 없어짐
