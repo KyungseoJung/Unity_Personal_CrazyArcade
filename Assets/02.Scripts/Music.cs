168abb17065c2b20bbf49a03e87adbe9dbf2db93
KyungseoJung <junggs1245@gmail.com>
2024-03-02 22:21:58 +0900

    feat(Music.cs): Make Music.cs script needed to play the game #18

---
+++ Assets/02.Scripts/Music.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // #18 Musics.cs 스크립트 생성
+    public AudioSource gameMusicArr;
+
+    void Awake()
+    {
+        gameMusicArr = gameObject.AddComponent<AudioSource>();  // #18 오디오소스 없기 때문에, 추가해서 지정해줘야 함
+
+    }
+
+}
