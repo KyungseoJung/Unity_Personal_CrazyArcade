a3580ab76a032fd1261fbc2bdc4565e15462e8b8
KyungseoJung <junggs1245@gmail.com>
2024-03-03 19:41:51 +0900

    feat(PlayerLife.cs): Set in the PlayerLife.cs script and animator that if the player touches the water balloon, the player will be trapped in the water balloon #17

--- Assets/02.Scripts/PlayerLife.cs
+++ Assets/02.Scripts/PlayerLife.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
+    private Animator anim;  // #17 플레이어 물풍선에 갇힐 때 - 애니메이션 설정
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();    // #17
+    }
+
     private void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "WaterWeapon")    // #17 플레이어가 물풍선에 닿으면 플레이어 물풍선에 갇힘
         {
             Debug.Log("//#17 플레이어 물풍선에 닿음. 갇힘.");
+            anim.SetBool("canMove", false);
+            anim.SetTrigger("trappedInWater");
         }
     }
     
