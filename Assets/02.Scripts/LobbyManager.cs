6dc179a947853dba749e98424d7f62bef524bc0c
KyungseoJung <junggs1245@gmail.com>
2024-03-04 10:11:26 +0900

    feat(LobbyManager.cs): Create and configure LobbyManager.cs to switch to scStage1-3D scene in scOpen #19

---
+++ Assets/02.Scripts/LobbyManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;          // #19 Scene 전환 목적
+
+public class LobbyManager : MonoBehaviour
+{
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame() // #19 시작하자마자 화면 전환
+    {
+        SceneManager.LoadScene("scStage1-3D");
+    }
+
+
+}
