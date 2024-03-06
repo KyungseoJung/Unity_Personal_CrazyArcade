3b07b2e8f68d15c52b36c5bf72ea9135952053f6
KyungseoJung <junggs1245@gmail.com>
2024-03-06 11:31:26 +0900

    feat(DontDestroy.cs): Add a script to prevent objects in the scOpen scene from being destroyed when changing scenes #19

---
+++ Assets/02.Scripts/DontDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()    
+    {
+        // #19 (씬 전환 시)이 게임오브젝트가 사라지지 않도록
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+}
