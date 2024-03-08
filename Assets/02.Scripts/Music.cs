41ece806a24908c5352100a9509fb93e8a4a0982
KyungseoJung <junggs1245@gmail.com>
2024-03-08 14:17:59 +0900

    feat(Music.cs): Have the main BGM run at the same time the game starts #20

--- Assets/02.Scripts/Music.cs
+++ Assets/02.Scripts/Music.cs
@@ -6,10 +6,35 @@ public class Music : MonoBehaviour
 {
     // #18 Musics.cs 스크립트 생성
     public AudioSource gameMusicArr;
+    public AudioClip[] audioClips;
+    /*
+    - 배경음 -
+    0 : BGSound1
+
+    - 효과음 -
+    
+    */
+
+    public enum BGM_TYPE {MAINMUSIC =1 }; // #20 메인 배경음
 
     void Awake()
     {
         gameMusicArr = gameObject.AddComponent<AudioSource>();  // #18 오디오소스 없기 때문에, 추가해서 지정해줘야 함
+
+    }
+
+    void Start()
+    {
+        BackGroundMusic(BGM_TYPE.MAINMUSIC);    // #20 메인 뮤직 BGM
+    }
+
+    public void BackGroundMusic(BGM_TYPE _type)
+    {   
+        Debug.Log("//#20 메인 배경음 시작");
+        gameMusicArr.Stop();
+        gameMusicArr.clip = audioClips[(int)_type -1];
+        gameMusicArr.Play();
+        gameMusicArr.loop = true;  // #20 메인 뮤직 BGM 반복되도록 설정
 
     }
 
