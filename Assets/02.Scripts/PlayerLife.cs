0573fa2e90bb3a9ccb4cf63b503f0135371231d8
KyungseoJung <junggs1245@gmail.com>
2024-03-31 14:21:02 +0900

    feat(PlayerLife.cs): When the player is trapped in a water balloon and dies when the water balloon bursts, set the 'trappedInWater' bool variable to false #28

--- Assets/02.Scripts/PlayerLife.cs
+++ Assets/02.Scripts/PlayerLife.cs
@@ -43,6 +43,8 @@ public class PlayerLife : MonoBehaviour
         // #28 플레이어가 물풍선에 갇힌 시간이 오래되면 - 죽는 애니메이션 재생 & 플레이어 죽음
         PlayerGameMgr.Mgr.life -=1;
         Debug.Log("//#28 플레이어 남은 목숨: " + PlayerGameMgr.Mgr.life);
+        
+        trappedInWater = false; // #28 물풍선이 터지면서 플레이어가 죽으면, 물풍선에 갇혀 있는지 확인하는 bool형 변수도 false로
     }
     
 }
