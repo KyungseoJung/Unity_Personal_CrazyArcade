3b5ab83c7f0c5bc71adddabe5e17fd505e85c372
KyungseoJung <junggs1245@gmail.com>
2024-05-12 14:08:10 +0900

    refactor(MapManager.cs):  In MapManger.cs, distinguish each variable by adding a dedicated variable that checks the row and col of each water balloon, obstacle, and item #4

--- Assets/02.Scripts/MapManager.cs
+++ Assets/02.Scripts/MapManager.cs
@@ -14,7 +14,9 @@ public class MapManager : MonoBehaviour
 
     private int waterballoonPlaceNum = 0;   // #13 맵에 놓여진 물풍선의 개수
 
-    int row, col;   // #4 선언 위치만 바꿈
+    int balloonRow, balloonCol;   // #4 선언 위치만 바꿈
+    int obsRow, obsCol;           // #4 장애물 전용 row, col
+    int itemRow, itemCol;         // #4 아이템 전용 row, col
     int playerRow, playerCol;   // #17
     int rowNum = 4; // #25
     int colNum = 3; // #25
@@ -100,32 +102,32 @@ public class MapManager : MonoBehaviour
         // #25 
         for(int i=0; i<obstacles.Length; i++)
         {
-            row = ReturnRowInMatrix(obstacles[i].transform.position.y);     // #26 함수 이용
-            col =  ReturnColInMatrix(obstacles[i].transform.position.x);    // #26 함수 이용
-            Debug.Log("//#25 Obstacle 존재 - row: " + row + ", col: " + col);
+            obsRow = ReturnRowInMatrix(obstacles[i].transform.position.y);     // #26 함수 이용
+            obsCol =  ReturnColInMatrix(obstacles[i].transform.position.x);    // #26 함수 이용
+            Debug.Log("//#25 Obstacle 존재 - obsRow: " + obsRow + ", obsCol: " + obsCol);
             Debug.Log("//#25 Ostacle 총 몇 개= " + (i + 1) + "오브젝트 이름: " + obstacles[i].gameObject.name);
-            obstacleArr[row, col] = 1;  // #25 
+            obstacleArr[obsRow, obsCol] = 1;  // #25 
 
         }
 
         // #25 아이템 배열
         for(int i=0; i<items.Length; i++)
         {
-            row = ReturnRowInMatrix(items[i].transform.position.y);     // #26 함수 이용
-            col =  ReturnColInMatrix(items[i].transform.position.x);    // #26 함수 이용
-            Debug.Log("//#25 Obstacle 존재 - row: " + row + ", col: " + col);
+            itemRow = ReturnRowInMatrix(items[i].transform.position.y);     // #26 함수 이용
+            itemCol =  ReturnColInMatrix(items[i].transform.position.x);    // #26 함수 이용
+            Debug.Log("//#25 Obstacle 존재 - itemRow: " + itemRow + ", itemCol: " + itemCol);
             Debug.Log("//#25 Ostacle 총 몇 개= " + (i + 1) + "오브젝트 이름: " + items[i].gameObject.name);
-            itemArr[row, col] = 1;  // #25 
+            itemArr[itemRow, itemCol] = 1;  // #25 
 
         }
     }
     
     public void RemoveItemPos(Transform _trans) // #10 아이템 획득하면 - 해당 아이템의 ItemArr을 0으로 전황
     {
-        row = ReturnRowInMatrix(_trans.position.y);     
-        col =  ReturnColInMatrix(_trans.position.x);    
+        itemRow = ReturnRowInMatrix(_trans.position.y);     
+        itemCol =  ReturnColInMatrix(_trans.position.x);    
         Debug.Log("// #10 아이템 획득 - 해당 위치 배열을 0으로 전환");
-        itemArr[row, col] = 0;  // #10 
+        itemArr[itemRow, itemCol] = 0;  // #10 
     }
 
     public void PlaceWaterBalloon(float _x, float _y)    // #4 플레이어 물풍선 놓기 - 받아오는 parameter는 플레이어의 좌표
@@ -139,18 +141,13 @@ public class MapManager : MonoBehaviour
         playerRow = ReturnRowInMatrix(_y);    // #26 함수 이용 
         playerCol = ReturnColInMatrix(_x);    // #26 함수 이용
 
-        Debug.Log("//#8 #4 물풍선 생성: row = " + playerRow + "/ col = " + playerCol);
+        Debug.Log("//#8 #4 물풍선 생성: balloonRow = " + playerRow + "/ balloonCol = " + playerCol);
 
-        if ((waterBalloonArr[playerRow, playerCol] == 1)) 
+        if ((waterBalloonArr[playerRow, playerCol] == 1) || (obstacleArr[playerRow, playerCol] == 1)) 
         {
             // 이미 그 자리에 물풍선 or 장애물이 있다면 - 중복해서 물풍선을 놓지 못하도록
-            Debug.Log("//#4 fix: 물풍선 이미 있음");
-            return;
-
-        }
-        else if((obstacleArr[playerRow, playerCol] == 1))
-        {
-            Debug.Log("//#4 fix 장애물 이미 있음");
+            // Debug.Log("//#4 fix: 물풍선 이미 있음");
+            // Debug.Log("//#4 fix 장애물 이미 있음");
             return;
         }
 
@@ -172,13 +169,13 @@ public class MapManager : MonoBehaviour
 
     public void RemoveWaterBalloon(float _x, float _y)  // #8 시간이 지남에 따라 물풍선 터짐 - 받아오는 parameter는 물풍선의 좌표
     {
-        Debug.Log("//#8 물풍선 소멸: row = " + row + "/ col = " + col);
+        Debug.Log("//#8 물풍선 소멸: balloonRow = " + balloonRow + "/ balloonCol = " + balloonCol);
 
-        row = ReturnRowInMatrix(_y);
-        col = ReturnColInMatrix(_x);
+        balloonRow = ReturnRowInMatrix(_y);
+        balloonCol = ReturnColInMatrix(_x);
 
-        waterBalloonArr[row, col] = 0;  // 배열 설정
-        obstacleArr[row, col] = 0;      // #25 배열 설정
+        waterBalloonArr[balloonRow, balloonCol] = 0;  // 배열 설정
+        obstacleArr[balloonRow, balloonCol] = 0;      // #25 배열 설정
 
         waterballoonPlaceNum -= 1; // #13 물풍선 개수 하나 감소
         music.SoundEffect(Music.EFFECT_TYPE.BUBBLE_BOOM); // #21 물풍선 터질 때의 효과음
@@ -190,32 +187,32 @@ public class MapManager : MonoBehaviour
     public void CheckPlayerTouchFluid(Transform _balloonTrans, Transform _playerTrans, int _waterLength)   
     // #17 플레이어가 물풍선의 물줄기와 닿았나 확인
     {
-        row = ReturnRowInMatrix(_balloonTrans.position.y);
-        col = ReturnColInMatrix(_balloonTrans.position.x);
+        balloonRow = ReturnRowInMatrix(_balloonTrans.position.y);
+        balloonCol = ReturnColInMatrix(_balloonTrans.position.x);
 
         playerRow = ReturnRowInMatrix(_playerTrans.position.y);
         playerCol = ReturnColInMatrix(_playerTrans.position.x);
 
         // 만약 플레이어가 터지는 물풍선과 같은 행이라면
         // && 플레이어와 물풍선과의 거리가 _waterLength보다 가깝다면
-        if((row == playerRow) && 
-            ((playerCol - col)*(playerCol - col) <= (_waterLength)*(_waterLength)))
+        if((balloonRow == playerRow) && 
+            ((playerCol - balloonCol)*(playerCol - balloonCol) <= (_waterLength)*(_waterLength)))
         {
             Debug.Log("//#17 같은 행 - 플레이어가 물줄기에 닿음");
             Debug.Log("//#17 물줄기 길이: " + _waterLength);
-            Debug.Log("//#17 물줄기와 플레이어 간의 거리: " + (playerCol-col));
+            Debug.Log("//#17 물줄기와 플레이어 간의 거리: " + (playerCol-balloonCol));
 
             playerLife.PlayerInWaterBalloon(); // #17 플레이어 물풍선에 갇힘
         }
 
         // 만약 플레이어가 터지는 물풍선과 같은 열이라면
         // && 플레이어와 물풍선과의 거리가 _waterLength보다 가깝다면
-        if((col == playerCol) &&
-            ((playerRow-row)*(playerRow-row) <= (_waterLength)*(_waterLength)))
+        if((balloonCol == playerCol) &&
+            ((playerRow-balloonRow)*(playerRow-balloonRow) <= (_waterLength)*(_waterLength)))
         {
             Debug.Log("//#17 같은 열 - 플레이어가 물줄기에 닿음");
             Debug.Log("//#17 물줄기 길이: " + _waterLength);
-            Debug.Log("//#17 물줄기와 플레이어 간의 거리: " + (playerRow-row));
+            Debug.Log("//#17 물줄기와 플레이어 간의 거리: " + (playerRow-balloonRow));
 
             playerLife.PlayerInWaterBalloon(); // #17 플레이어 물풍선에 갇힘
         }
@@ -235,33 +232,33 @@ public class MapManager : MonoBehaviour
     public void CheckBubbleTouchFluid(Transform _balloon, int _waterLength)
     // #31 물풍선의 물줄기가 다른 물풍선에 닿았나 확인
     {
-        row = ReturnRowInMatrix(_balloon.position.y);
-        col = ReturnColInMatrix(_balloon.position.x);
+        balloonRow = ReturnRowInMatrix(_balloon.position.y);
+        balloonCol = ReturnColInMatrix(_balloon.position.x);
         // #31 물풍선의 상하좌우 파악
         for(int i=0; i<_waterLength; i++)
         {
-            if(waterBalloonArr[row-i-1, col]==1)    // 물풍선의 상(위)에 다른 물풍선이 있는지 파악
+            if(waterBalloonArr[balloonRow-i-1, balloonCol]==1)    // 물풍선의 상(위)에 다른 물풍선이 있는지 파악
             {
                 Debug.Log("//#31 물풍선의 물줄기가 다른 \"위쪽\" 물풍선에 닿음");
                 CheckIsThereWaterBalloon(_balloon.position.x, _balloon.position.y+i+1); // #32
             }
-            if(waterBalloonArr[row+i+1, col]==1)    // 물풍선의 하(아래)
+            if(waterBalloonArr[balloonRow+i+1, balloonCol]==1)    // 물풍선의 하(아래)
             {
                 Debug.Log("//#31 물풍선의 물줄기가 다른 \"아래쪽\" 물풍선에 닿음");
                 CheckIsThereWaterBalloon(_balloon.position.x, _balloon.position.y-i-1); // #32
             }
-            if(waterBalloonArr[row, col-i-1]==1)    // 물풍선의 좌(왼쪽)
+            if(waterBalloonArr[balloonRow, balloonCol-i-1]==1)    // 물풍선의 좌(왼쪽)
             {
                 Debug.Log("//#31 물풍선의 물줄기가 다른 \"왼쪽\" 물풍선에 닿음");
-                Debug.Log("//#31 터진 물풍선 위치| 행: " + row + ", 열: " + col);
-                Debug.Log("//#31 물줄기에 맞은 물풍선 위치| 행:  "+ row + ", 열: " + (col-i-1));
+                Debug.Log("//#31 터진 물풍선 위치| 행: " + balloonRow + ", 열: " + balloonCol);
+                Debug.Log("//#31 물줄기에 맞은 물풍선 위치| 행:  "+ balloonRow + ", 열: " + (balloonCol-i-1));
                 CheckIsThereWaterBalloon(_balloon.position.x-i-1, _balloon.position.y); // #32
             }
-            if(waterBalloonArr[row, col+i+1]==1)    // 물풍선의 우(오른쪽)
+            if(waterBalloonArr[balloonRow, balloonCol+i+1]==1)    // 물풍선의 우(오른쪽)
             {
                 Debug.Log("//#31 물풍선의 물줄기가 다른 \"오른쪽\" 물풍선에 닿음");
-                Debug.Log("//#31 터진 물풍선 위치| 행: " + row + ", 열: " + col);
-                Debug.Log("//#31 물줄기에 맞은 물풍선 위치| 행:  "+ row + ", 열: " + (col-i-1));
+                Debug.Log("//#31 터진 물풍선 위치| 행: " + balloonRow + ", 열: " + balloonCol);
+                Debug.Log("//#31 물줄기에 맞은 물풍선 위치| 행:  "+ balloonRow + ", 열: " + (balloonCol-i-1));
                 CheckIsThereWaterBalloon(_balloon.position.x+i+1, _balloon.position.y); // #32
             }
         }
