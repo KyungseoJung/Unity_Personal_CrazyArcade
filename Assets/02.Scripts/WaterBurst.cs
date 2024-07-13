ce5ec3be3f0ec22efd1043d4f854a5768e215198
KyungseoJung <junggs1245@gmail.com>
2024-07-13 14:10:23 +0900

    feat(WaterBurst.cs): When the lower water balloon (ex. up2) hits an obstacle, the lower water balloon is deactivated, and the upper water balloon (ex. up1) is also deactivated #37

--- Assets/02.Scripts/WaterBurst.cs
+++ Assets/02.Scripts/WaterBurst.cs
@@ -7,10 +7,13 @@ public class WaterBurst : MonoBehaviour
     private int row;
     private int col;
     private MapManager mapMgr;  // #37 좌표 값을 행렬로 변환하기 위함 & obstacleArr 확인하기 위함
+    [SerializeField] private GameObject topWaterBurst;   // #37 up2 오브젝트의 상위 오브젝트는 up1
+                // 하위 물 폭탄 오브젝트(up2)가 비활성화 되면, 상위 물 폭탄 오브젝트(up1)도 비활성화 하기 위함
 
     private void Awake()
     {
         mapMgr = GameObject.FindGameObjectWithTag("MapManager").GetComponent<MapManager>(); // #37
+        // topWaterBurst = transform.parent.Find(this.gameObejct.name + "")
     }
 
     private void Start()
@@ -36,6 +39,12 @@ public class WaterBurst : MonoBehaviour
         {
             this.gameObject.SetActive(false);
             Debug.Log("//#37 " + this.gameObject.name + "물폭탄이 Obstacle에 닿음");
+
+            if(topWaterBurst != null)   
+            {
+                topWaterBurst.SetActive(false); 
+                Debug.Log("//#37 " + topWaterBurst.name + "물폭탄도 비활성화하기");
+            }
         } 
     }
 }
