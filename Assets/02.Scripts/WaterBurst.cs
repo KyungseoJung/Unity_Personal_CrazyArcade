9fa2e5e47754aaf97dc406eeabbb908608d010c1
KyungseoJung <junggs1245@gmail.com>
2024-07-14 00:37:37 +0900

    fix(Obj_WaterBalloon_5.prefab, WaterBurst.cs): Change the location of WaterBurst.cs to the parent object (waterUp) so that it can be managed comprehensively by the parent object #37

--- Assets/02.Scripts/WaterBurst.cs
+++ Assets/02.Scripts/WaterBurst.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class WaterBurst : MonoBehaviour
 {
-    private int row;
-    private int col;
+    private int firstRow;
+    private int firstCol;
+    private int secondRow;
+    private int secondCol;
     private MapManager mapMgr;  // #37 좌표 값을 행렬로 변환하기 위함 & obstacleArr 확인하기 위함
-    [SerializeField] private GameObject topWaterBurst;   // #37 up2 오브젝트의 상위 오브젝트는 up1
+    [SerializeField] private GameObject firstWaterObj;   // #37 waterUp 오브젝트 자식들 중 상위 오브젝트는 up1
                 // 하위 물 폭탄 오브젝트(up2)가 비활성화 되면, 상위 물 폭탄 오브젝트(up1)도 비활성화 하기 위함
+    [SerializeField] private GameObject secondWaterObj;   // #37 waterUp 오브젝트 자식들 중 하위 오브젝트는 up2
 
     private void Awake()
     {
         mapMgr = GameObject.FindGameObjectWithTag("MapManager").GetComponent<MapManager>(); // #37
         // topWaterBurst = transform.parent.Find(this.gameObejct.name + "")
+        Debug.Log("상위 오브젝트: " + firstWaterObj.name);
+        Debug.Log("하위 오브젝트: " + secondWaterObj.name);
     }
 
     private void Start()
     {
         // Debug.Log("//#37 위치: " + this.transform.position);
-        CheckObstacle();
+        CheckFirstWater();
+        CheckSecondWater();
     }
 
     // private void OnTriggerEnter(Collider other)
@@ -30,21 +36,30 @@ public class WaterBurst : MonoBehaviour
     //     }
     // }
 
-    private void CheckObstacle()
+    private void CheckFirstWater()
     {
-        row = mapMgr.ReturnRowInMatrix(this.transform.position.y);
-        col = mapMgr.ReturnColInMatrix(this.transform.position.x);
+        firstRow = mapMgr.ReturnRowInMatrix(firstWaterObj.transform.position.y);
+        firstCol = mapMgr.ReturnColInMatrix(firstWaterObj.transform.position.x);
 
-        if(mapMgr.obstacleArr[row, col] == 1)
+        if(mapMgr.obstacleArr[firstRow, firstCol] == 1)   // 만약 하위 물줄기가 장애물에 닿으면, 하위 물줄기도 비활성화 하고, 상위 물줄기도 비활성화하기
         {
-            this.gameObject.SetActive(false);
-            Debug.Log("//#37 " + this.gameObject.name + "물폭탄이 Obstacle에 닿음");
+            firstWaterObj.SetActive(false);
+            Debug.Log("//#37 " + firstWaterObj.name + "물폭탄 비활성화 | Obstacle에 닿음");
 
-            if(topWaterBurst != null)   
-            {
-                topWaterBurst.SetActive(false); 
-                Debug.Log("//#37 " + topWaterBurst.name + "물폭탄도 비활성화하기");
-            }
-        } 
+        }
+    }
+    private void CheckSecondWater()
+    {
+        secondRow = mapMgr.ReturnRowInMatrix(secondWaterObj.transform.position.y);
+        secondCol = mapMgr.ReturnColInMatrix(secondWaterObj.transform.position.x);
+
+        if(mapMgr.obstacleArr[secondRow, secondCol] == 1)   // 만약 하위 물줄기가 장애물에 닿으면, 하위 물줄기도 비활성화 하고, 상위 물줄기도 비활성화하기
+        {
+            secondWaterObj.SetActive(false);
+            Debug.Log("//#37 " + secondWaterObj.name + "물폭탄 비활성화 | Obstacle에 닿음");
+
+            firstWaterObj.SetActive(false); 
+            Debug.Log("//#37 " + firstWaterObj.name + "물폭탄 비활성화");
+        }
     }
 }
