d51675d9126853636bc9ec48d2eb34e878d9e2f1
KyungseoJung <junggs1245@gmail.com>
2024-07-16 18:40:06 +0900

    feat(Obj_WaterBalloon_5 prefab, WaterBurst.cs): In the water balloon, Activate the upper object (ex. waterUp) and deactivate the lower object (ex. up1) so that the moment player place the water balloon, the Awake of the WaterBurst script is executed & Connect the collider of the lower objects (ex. up1) in the WaterBurst script #37

--- Assets/02.Scripts/WaterBurst.cs
+++ Assets/02.Scripts/WaterBurst.cs
@@ -11,7 +11,10 @@ public class WaterBurst : MonoBehaviour
     private MapManager mapMgr;  // #37 좌표 값을 행렬로 변환하기 위함 & obstacleArr 확인하기 위함
     [SerializeField] private GameObject firstWaterObj;   // #37 waterUp 오브젝트 자식들 중 상위 오브젝트는 up1
                 // 하위 물 폭탄 오브젝트(up2)가 비활성화 되면, 상위 물 폭탄 오브젝트(up1)도 비활성화 하기 위함
+    [SerializeField] private BoxCollider firstWatObjCol;     // #37 콜라이더 처리가 먼저 되지 않도록, 장애물이 없는 걸 확인한 후에 각 오브젝트의 BoxCollider 활성화하기
     [SerializeField] private GameObject secondWaterObj;   // #37 waterUp 오브젝트 자식들 중 하위 오브젝트는 up2
+
+    [SerializeField] private BoxCollider secondWatObjCol;    // #37 콜라이더 처리가 먼저 되지 않도록, 장애물이 없는 걸 확인한 후에 각 오브젝트의 BoxCollider 활성화하기
 
     private void Awake()
     {
@@ -19,6 +22,37 @@ public class WaterBurst : MonoBehaviour
         // topWaterBurst = transform.parent.Find(this.gameObejct.name + "")
         Debug.Log("상위 오브젝트: " + firstWaterObj.name);
         Debug.Log("하위 오브젝트: " + secondWaterObj.name);
+
+        // #37 콜라이더 처리가 먼저 되지 않도록, 장애물이 없는 걸 확인한 후에 각 오브젝트의 BoxCollider 활성화하기
+        // firstWatObjCol = firstWaterObj.GetComponent<BoxCollider>();
+        // secondWatObjCol = secondWaterObj.GetComponent<BoxCollider>();
+
+        if (firstWaterObj == null)
+        {
+            Debug.LogError("firstWaterObj가 null입니다.");
+        }
+        else
+        {
+            firstWatObjCol = firstWaterObj.GetComponent<BoxCollider>();
+            if (firstWatObjCol == null)
+            {
+                Debug.LogError("firstWaterObj의 BoxCollider를 찾을 수 없습니다.");
+            }
+        }
+
+        if (secondWaterObj == null)
+        {
+            Debug.LogError("secondWaterObj가 null입니다.");
+        }
+        else
+        {
+            secondWatObjCol = secondWaterObj.GetComponent<BoxCollider>();
+            if (secondWatObjCol == null)
+            {
+                Debug.LogError("secondWaterObj의 BoxCollider를 찾을 수 없습니다.");
+            }
+        }
+
     }
 
     private void Start()
@@ -45,7 +79,6 @@ public class WaterBurst : MonoBehaviour
         {
             firstWaterObj.SetActive(false);
             Debug.Log("//#37 " + firstWaterObj.name + "물폭탄 비활성화 | Obstacle에 닿음");
-
         }
     }
     private void CheckSecondWater()
