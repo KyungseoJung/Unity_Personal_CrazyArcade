34115b120a556cb048c117157737bb08bb18c812
KyungseoJung <junggs1245@gmail.com>
2024-09-29 15:34:24 +0900

    fix(Obstacle.cs): When detecting obstacles, check blockArr to solve the problem of WoodBlock being pushed by overlapping blocks #14

--- Assets/02.Scripts/Obstacle.cs
+++ Assets/02.Scripts/Obstacle.cs
@@ -273,7 +273,9 @@ public class Obstacle : MonoBehaviour
             // #14 아예 배열 바운더리를 넘어가도 장애물 있는 것으로 판단
             return true;
         }
-        else if((mapMgr.obstacleArr[row, col] ==1) || (mapMgr.itemArr[row, col] ==1) || (mapMgr.waterBalloonArr[row, col]==1))
+        else if((mapMgr.obstacleArr[row, col] ==1) || (mapMgr.itemArr[row, col] ==1)
+         || (mapMgr.waterBalloonArr[row, col]==1) || (mapMgr.blockArr[row, col]==1))    
+         // #14 fix: 고려되지 않았던 blockArr도 파악한 후, WOODBLOCK 밀기
         {
             // IndexOutOfRangeException 에러를 피하기 위해 바운더리 넘어간 것부터 확인 후, 배열 확인
             return true;
