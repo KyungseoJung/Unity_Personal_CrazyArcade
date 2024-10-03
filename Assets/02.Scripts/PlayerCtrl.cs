ee19b1a5d507dbfb3b06cf42dc71c36a62c5922f
KyungseoJung <junggs1245@gmail.com>
2024-10-04 02:36:58 +0900

    feat(PlayerCtrl.cs): When the player escapes the water balloon, reset it to its original speed #41

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -139,7 +139,7 @@ public class PlayerCtrl : MonoBehaviour // #1
         {
             if((playerLife.trappedInWater) && (PlayerGameMgr.Mgr.needle > 0))
             {
-                // 만약 물풍선에 갇혀 있을 때, 키보드의 숫자 '2'를 누르면~
+                // #41 만약 물풍선에 갇혀 있을 때, 키보드의 숫자 '2'를 누르면~ 물풍선에서 벗어나도록
                 playerLife.trappedInWater = false;  // #41 물풍선 벗어나도록
                 playerLife.waterApplied = false;    // #17 fix: 플레이어가 물풍선 벗어날 때, 변수 'waterApplied' 를 false로 설정하라 - 한번 물풍선 탈출하면, 그 이후에 물풍선 적용이 안 되는 문제 해결하기 위해
                 anim.SetTrigger("EscapeWater");     // #41 물풍선 벗어나는 애니메이션 실행 뒤, 기존 PlayerLookingAhead 애니메이션 실행
@@ -147,6 +147,9 @@ public class PlayerCtrl : MonoBehaviour // #1
                 music.GameSoundEffect(Music.EFFECT_TYPE.BOMB_POP, 0.6f);    // #43 바늘 아이템 사용해서 물풍선 벗어날 때 효과음
                 music.StopPlayerSoundEffect();  // #47 플레이어에게 적용되었던 'PLYAER_IN_BALLOON' 효과음 멈추기
                 PlayerGameMgr.Mgr.needle -= 1;      // #43 바늘 아이템 사용
+
+                ChangePlayerSpeed(PlayerGameMgr.Mgr.roller); // #41 플레이어가 물풍선에서 벗어나면, 본래 속도로 돌아가도록 - 획득한 roller 아이템을 바탕으로 본래 속도로 돌아가기
+
             }
             Debug.Log("//#41 키보드의 숫자 '2'를 누름");  
         }
