c2b285302e9fb94abbd97c2a4eea35e718991f46
KyungseoJung <junggs1245@gmail.com>
2024-10-11 01:33:59 +0900

    feat(LobbyManager.cs): Change the method of executing a specific function when a button is pressed, as set in the Inspector, to the method of implementing it in code #49

--- Assets/02.Scripts/LobbyManager.cs
+++ Assets/02.Scripts/LobbyManager.cs
@@ -10,9 +10,22 @@ public class LobbyManager : MonoBehaviour
 {
     public Text txtPlayerLife;                    // #27 플레이어 목숨 표시
 
+    [SerializeField] Button btnHowToGame;           // #49 '게임 방법' 버튼
+    [SerializeField] Button btnStartGame;           // #49 '게임 시작' 버튼
+
     void Start()
     {
         // StartGame(); // #49 '게임 시작' 버튼 누르면, StartGame() 함수 실행되도록 하기
+        // #49 특정 버튼에 대해 함수를 연결하는 부분을 인스펙터(Inspector)에서 했었음. -> 코드상으로 설정하는 방식으로 변경하기.
+        if(btnHowToGame != null)
+        {
+            btnHowToGame.onClick.AddListener(ShowHowToGame);
+        }
+
+        if(btnStartGame != null)
+        {
+            btnStartGame.onClick.AddListener(StartGame);
+        }
     }
 
     public void StartGame() // #19 시작하자마자 화면 전환
