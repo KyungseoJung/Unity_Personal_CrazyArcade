d40e4b1e2c520b3e140a37a924b541c9ae483411
KyungseoJung <junggs1245@gmail.com>
2024-12-09 00:55:26 +0900

    fix(PlayerLife.cs): When a player first appears, make sure player cannot place a water balloon until they are fully present #14

--- Assets/02.Scripts/PlayerLife.cs
+++ Assets/02.Scripts/PlayerLife.cs
@@ -23,7 +23,7 @@ public class PlayerLife : MonoBehaviour
     float mapPlaceY;
     
     public bool trappedInWater = false;    // #17 플레이어 물풍선에 갇혔는지 확인용 bool형 변수
-    public bool playerFaint = false;       // #28 플레이어 기절했는지 확인
+    public bool playerFaint;       // #28 플레이어 기절했는지 확인
     private bool playerDie = false;         // #28 플레이어가 완전히 죽었는지 확인 (목숨 모두 소진)
     public bool waterApplied = false;      // #17 fix: 이미 물풍선이 적용되었는지 확인용 bool형 변수
 
@@ -38,6 +38,7 @@ public class PlayerLife : MonoBehaviour
 
     void Start()
     {
+        playerFaint = true; // #14 처음 시작할 때, 기절한 상태로 시작. 완전히 등장하면, 그때 기절 상태 false로 만들기. 물풍선 놓을 수 없도록 하기 위해
         respawnPos = new Vector3(4, -3, -0.05f); // #29 리스폰 위치 지정
         // anim.SetBool("canMove", true);   // #17 #19 fix(주석 처리) 첫 설정은 true로 해서 애니메이션 정상 작동하도록
         anim.SetBool("canMove", false);     // #19 fix: 처음에는 플레이어가 등장하는 애니메이션 (PlayerSpin)을 위해서 "canMove"를 false로 설정. PlayerSpin 애니메이션이 끝나면 PlayerCanMove함수를 이용해서 "canMove"를 true로 설정.
@@ -269,7 +270,8 @@ public class PlayerLife : MonoBehaviour
     private void PlayerCanMove()    // #19 fix: PlayerSpin 애니메이션이 끝나면 PlayerCanMove함수를 이용해서 "canMove"를 true로 설정.
     {
         anim.SetBool("canMove", true);
-
+        
+        playerFaint = false;    // #14 처음 시작할 때, 기절한 상태로 시작. 완전히 등장하면, 그때 기절 상태 false로 만들기. 물풍선 놓을 수 없도록 하기 위해
     }
     private void PlayerCanMoveLookingAhead()    // #41 fix: PlayerEscapeWater 애니메이션 종료되는 시점에 실행 - 물풍선 탈출 애니메이션 후에, 원래 애니메이션이 정상적으로 작동하도록
     {
