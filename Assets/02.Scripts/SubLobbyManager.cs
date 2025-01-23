3ccdb1159175a7ddeb1735e54cf8801109ec4abe
KyungseoJung <junggs1245@gmail.com>
2025-01-23 20:07:24 +0900

    feat(SubLobbyManager.cs): Add a "SubLobbyManager.cs" script for Player2 #101

---
+++ Assets/02.Scripts/SubLobbyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;                       // #101 플레이어 목숨 표시
+
+public class SubLobbyManager : MonoBehaviour
+{
+    // #101 여기부터 - UI 이미지  ========================================
+    public Text txtPlayerLife;                    // 플레이어 목숨 표시
+    public Text txtNumberOfCan;                    // 느린 거북을 빠르게 해주는 can item
+    public Text txtNumberOfNeedle;                 // 물풍선에 갇혔을 때, 벗어나게 해주는 needle item
+    public Text txtNumberOfShield;                 // 외부 공격으로부터 막아주는 shield item
+
+    void Start()
+    {
+        // #101 여기부터 - UI 이미지  ========================================
+        txtNumberOfCan.text = $"{PlayerGameMgr.Mgr.turtleCan}";
+        txtNumberOfNeedle.text = $"{PlayerGameMgr.Mgr.needle}";
+        txtNumberOfShield.text = $"{PlayerGameMgr.Mgr.shield}";
+    }
+
+    public void UpdateNumberOfItems()  //#101 UI에 나타나는 아이템의 개수 업데이트
+    {
+        txtNumberOfCan.text = $"{PlayerGameMgr.Mgr.turtleCan}";
+        txtNumberOfNeedle.text = $"{PlayerGameMgr.Mgr.needle}";
+        txtNumberOfShield.text = $"{PlayerGameMgr.Mgr.shield}";   
+    }
+}
