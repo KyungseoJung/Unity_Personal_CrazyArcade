ebf4c8360f35387d60c2ce871eff6a1f7f8d2aa3
KyungseoJung <junggs1245@gmail.com>
2025-02-28 14:19:23 +0900

    fix(SubPlayerLife.cs): Change the KeyCode for using Player2's shield item (0 to 3) #100

--- Assets/02.Scripts/SubPlayerLife.cs
+++ Assets/02.Scripts/SubPlayerLife.cs
@@ -50,7 +50,7 @@ public class SubPlayerLife : MonoBehaviour
     void Update()
     {
         // 방패 아이템 사용 - 외부 공격으로부터 막아주는 shield item
-        if(!trappedInWater && Input.GetKeyDown(KeyCode.Alpha0) && (SubPlayerGameMgr.SubMgr.shield>0)) // 물풍선에 갇혀있을 땐 사용하지 못하도록
+        if(!trappedInWater && Input.GetKeyDown(KeyCode.Alpha3) && (SubPlayerGameMgr.SubMgr.shield>0)) // 물풍선에 갇혀있을 땐 사용하지 못하도록
         {
             SubPlayerGameMgr.SubMgr.shield -= 1;      // 방패 아이템 사용
             lobbyMgr.UpdateNumberOfItems();     // 아이템 개수 업데이트해서 UI에 표시
@@ -63,10 +63,10 @@ public class SubPlayerLife : MonoBehaviour
     {
         if((other.gameObject.tag == "WaterBurst"))
         {
-            PlayerInWaterBalloon(); // 플레이어 물풍선에 갇힘
+            PlayerInWaterBalloon(); // 플레이어 물풍선에 닿음
         }
     }
-    public void PlayerInWaterBalloon() // 플레이어가 물풍선에 갇힘
+    public void PlayerInWaterBalloon() // 플레이어가 물풍선에 닿음
     {
         if(!trappedInWater && !waterApplied && !playerInvincible)   // 플레이어가 무적 상태이면 물풍선 맞아도 아무 영향 받지 않도록
         {
